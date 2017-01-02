3e17c8a1d8be4a2efcb695406a8c1518c3a03444
Christoph Küpker <christoph.kuepker@googlemail.com>
2017-01-02 12:37:20 +0100

    Initial commit

---
+++ D3arDiablo/ViewModel/BuildTabViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D3arDiablo.ViewModel
+{
+  public class BuildTabViewModel : ViewModelBase
+  {
+    public BuildTabViewModel(string title)
+    {
+      Title = title;
+    }
+
+    public string Title { get; set; }
+
+    public ICollection<BuildViewModel> Build { get; set; }
+  }
+}
---
+++ D3arDiablo/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.ObjectModel;
+
+namespace D3arDiablo.ViewModel
+{
+  public class MainWindowViewModel : ViewModelBase
+  {
+    public MainWindowViewModel()
+    {
+      BuildTabs = new ObservableCollection<BuildTabViewModel>
+      {
+        new BuildTabViewModel("Default Build")
+      };
+    }
+
+    public ObservableCollection<BuildTabViewModel> BuildTabs
+    {
+      get; set; 
+    }
+  }
+}

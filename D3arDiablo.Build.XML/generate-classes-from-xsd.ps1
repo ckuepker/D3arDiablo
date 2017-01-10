acefdf5a0b9553ab25a8842f999c4fbe01b18410
Christoph Küpker <christoph.kuepker@googlemail.com>
2017-01-10 22:42:29 +0100

    Generated classes from BuildSchema

---
+++ D3arDiablo.Build.XML/generate-classes-from-xsd.ps1
@@ -0,0 +1,4 @@
+﻿# This script imports the BuildExample.xsd and generates C# classes into the project source
+$xsd = 'Resources/BuildSchema.xsd'
+
+& "C:\Program Files (x86)\Microsoft SDKs\Windows\v8.1A\bin\NETFX 4.5.1 Tools\xsd.exe" /classes /namespace:D3arDiablo.Build.XML $xsd

8c712ecc1a1315daccfc2ffba2a6eee1a4120ed1
Christoph Küpker <christoph.kuepker@googlemail.com>
2017-01-13 22:41:50 +0100

    Improved test coverage for StorageManager

--- D3arDiablo.Storage.Test/StorageManagerTest.cs
+++ D3arDiablo.Storage.Test/StorageManagerTest.cs
@@ -12,19 +12,47 @@ namespace D3arDiablo.Storage.Test
   {
     private IStorageManager _manager;
     private readonly string _testLocation = TestContext.CurrentContext.TestDirectory + "/teststorage.xml";
+    private string _workingDirectory;
 
     [SetUp]
     public void Setup()
     {
+      _workingDirectory = Environment.CurrentDirectory;
+      if (File.Exists(_testLocation))
+      {
+        File.Delete(_testLocation);
+      }
       StorageSettings.StorageDefaultLocation = TestContext.CurrentContext.TestDirectory +
                                                "/Resources/BuildExample.xml";
-      _manager = new StorageManager();
     }
+
+    [TearDown]
+    public void TearDown()
+    {
+      File.Delete(_testLocation);
+      Environment.CurrentDirectory = _workingDirectory;
+    }
+
     [Test]
     public void TestLoadFromDefaultLocation()
     {
+      _manager = new StorageManager();
       IDictionary<CharacterClass, IEnumerable<IBuild>> result = _manager.LoadFromDefaultLocation();
       Assert.AreEqual(result[CharacterClass.Crusader].First().Name, "LoN");
+    }
+
+    [Test]
+    public void TestLoadFromDefaultLocationWithCreateDefault()
+    {
+      Assert.IsFalse(File.Exists(_testLocation), "Test file should be deleted initially");
+      StorageSettings.StorageDefaultLocation = _testLocation;
+
+      Environment.CurrentDirectory = TestContext.CurrentContext.TestDirectory;
+      _manager = new StorageManager();
+      IDictionary<CharacterClass, IEnumerable<IBuild>> result = _manager.LoadFromDefaultLocation();
+
+      Assert.IsTrue(File.Exists(_testLocation), "Test file should have been created");
+      Assert.AreEqual("Lashing Tail Kick Sunwoko", result[CharacterClass.Monk].First().Name);
     }
   }
 }
